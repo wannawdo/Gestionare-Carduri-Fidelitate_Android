d3e283307984c2c52f5fba41c9656b6febee2516
Oana Dragomirescu <dragomirescuoana18@stud.ase.ro>
2020-12-29 15:00:48 +0200

    facut pagina adaugare card

--- testProiect/testProiect/app/src/main/java/com/example/testproiect/AdaugareCardActivity.java
+++ testProiect/testProiect/app/src/main/java/com/example/testproiect/AdaugareCardActivity.java
@@ -21,9 +21,7 @@ public class AdaugareCardActivity extends AppCompatActivity {
     private EditText etNume, etIDCard;
     //final buton adauga
 
-    //pentru dateTimePicker
-    private TextView mDisplayDate;
-    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,31 +31,6 @@ public class AdaugareCardActivity extends AppCompatActivity {
         etNume = findViewById(R.id.etNume);
         etIDCard = findViewById(R.id.etIDCard);
 
-        mDisplayDate = (TextView) findViewById(R.id.tvAdaugaData);
-
-        mDisplayDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar calendar = Calendar.getInstance();
-                int year = calendar.get(Calendar.YEAR);
-                int month = calendar.get(Calendar.MONTH);
-                int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog dialog = new DatePickerDialog(AdaugareCardActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, mDateSetListener, year, month, day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
-            }
-        });
-
-        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                month = month + 1; //luna apare cu 1 in minus asa ca ii adaug 1
-                Log.d(TAG, "onDateSet: mm/dd/yyyy " + month + "/" + dayOfMonth + "/" + year);
-                String date = month + "/" + dayOfMonth + "/" + year;
-                mDisplayDate.setText(date);
-            }
-        };
 
     }
 }
